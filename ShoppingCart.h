6a807266f6c2a747b5038521c318c68049a5ac84
Justin Johnson <justinjohnson623@icloud.com>
2024-11-21 15:14:24 -0600

    Create ShoppingCart.h

---
+++ ShoppingCart.h
@@ -0,0 +1,31 @@
+#ifndef SHOPPINGCART_H
+#define SHOPPINGCART_H
+
+#include <string>
+#include <vector>
+#include "ItemToPurchase.h"
+
+using namespace std;
+
+class ShoppingCart {
+public:
+    ShoppingCart(string customerName = "none", string date = "January 1, 2016");
+
+    string GetCustomerName() const;
+    string GetDate() const;
+
+    void AddItem(const ItemToPurchase& item);
+    void RemoveItem(string itemName);
+    void ModifyItem(const ItemToPurchase& item);
+    int GetNumItemsInCart() const;
+    int GetCostOfCart() const;
+    void PrintTotal() const;
+    void PrintDescriptions() const;
+
+private:
+    string customerName;
+    string currentDate;
+    vector<ItemToPurchase> cartItems;
+};
+
+#endif
