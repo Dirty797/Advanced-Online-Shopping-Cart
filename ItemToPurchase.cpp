4768a05efcd7c02a49385e829466718720d72abd
Justin Johnson <justinjohnson623@icloud.com>
2024-11-21 15:13:33 -0600

    Create ItemToPurchase.cpp

---
+++ ItemToPurchase.cpp
@@ -0,0 +1,30 @@
+#include "ItemToPurchase.h"
+#include <iostream>
+
+using namespace std;
+
+// Parameterized constructor
+ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity)
+    : itemName(name), itemDescription(description), itemPrice(price), itemQuantity(quantity) {}
+
+void ItemToPurchase::SetName(string name) { itemName = name; }
+string ItemToPurchase::GetName() const { return itemName; }
+
+void ItemToPurchase::SetDescription(string description) { itemDescription = description; }
+string ItemToPurchase::GetDescription() const { return itemDescription; }
+
+void ItemToPurchase::SetPrice(int price) { itemPrice = price; }
+int ItemToPurchase::GetPrice() const { return itemPrice; }
+
+void ItemToPurchase::SetQuantity(int quantity) { itemQuantity = quantity; }
+int ItemToPurchase::GetQuantity() const { return itemQuantity; }
+
+void ItemToPurchase::PrintItemCost() const {
+    cout << itemName << " " << itemQuantity << " @ $" << itemPrice
+         << " = $" << (itemPrice * itemQuantity) << endl; 
+    
+}
+
+void ItemToPurchase::PrintItemDescription() const {
+    cout << itemName << ": " << itemDescription << endl;
+}
